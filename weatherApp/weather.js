f51554ada63b6fb601b712ee70a0f5ccb135bb14
Shahzeb Waleed <135195892+waleeddcoder@users.noreply.github.com>
2023-10-30 14:42:44 +0500

    Add files via upload

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -41,7 +41,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
 };
 
 const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}
     `;
 
     fetch(WEATHER_API_URL)
@@ -81,7 +81,7 @@ const getCityCoordinates = () => {
 
     const cityName = cityInput.value.trim();
     if (!cityName) return;
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}
     `;
     fetch(GEOCODING_API_URL)
         .then((res) => res.json())
@@ -103,7 +103,7 @@ const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
         (position) => {
             const { latitude, longitude } = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}
+            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}
             `;
             fetch(REVERSE_GEOCODING_URL)
                 .then((res) => res.json())
